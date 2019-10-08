83b84a50524a135957e18beeece5cb855644692e
Nick Burniske <nicoburniske@gmail.com>
2019-10-08 16:50:50 -0400

    minor changes to store notes module, mostly comments

--- src/store/modules/notes.js
+++ src/store/modules/notes.js
@@ -5,15 +5,16 @@ const state = {
 };
 
 const getters = {
-  getNotes: (state) => state.notes
+  getNotes: (state) => state.notes //should we send a copy?
 };
 
 const mutations = {
   saveNote(state, note) {
-    state.notes.push(note);
+    //state.notes.push(note);   //is mutation the best option here?
+    state.notes = [...state.notes, note];
   },
   deleteNote(state, noteIndex) {
-    state.notes.splice(noteIndex, 1);
+    state.notes.splice(noteIndex, 1); 
   },
   editNoteSaved(state, payload) {
     state.notes.splice(payload.index, 1, payload.note);
@@ -23,20 +24,21 @@ const mutations = {
 
 const actions = {
   //context contains the following: commit, state, getters, dispatch.
-  //I deconstructed the object to only have access to commit, since it's the only thing i need.
+  //I deconstructed the object to only have access to commit, since it's the only thing I need.
   async retrieveNotes({ commit }, numComments = 5) {
     try {
       let url = `https://jsonplaceholder.typicode.com/posts?_limit=${numComments}`;
       let notesRetrieved = await axios.get(url);
+      //commit('saveNote', ...notesRetrieved.data) this would work but the keys don't match up
       notesRetrieved.data.forEach(note =>
         commit('saveNote', {
           title: note.title,
           content: note.body
         })
-      )
+      );
     } catch (e) {
       console.log(e);
-      console.log("Could not retrieve notes")
+      console.log("Could not retrieve notes");
     }
   }
 };

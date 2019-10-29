d18f73846e243317403d9094df59dd30116b9f36
Nick Burniske <nicoburniske@gmail.com>
2019-10-29 16:34:39 -0400

    refactored a majority of store module to account for usage of axios interceptors

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,61 +1,74 @@
-import axios from 'axios';
-
-const axiosAPI = axios.create({
-  baseURL: '',
-  timeout: 5000,
-})
+/* eslint-disable no-shadow */
+import axiosInstance from '../../auth/axiosInstance'
+import { API_LOGIN, 
+  TOKEN_REFRESH_REQUEST,TOKEN_REFRESH_SUCCESS, TOKEN_REFRESH_FAILURE, 
+  LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE } from '../../auth/constants'
 
 const state = {
   status : '',
-  token : localStorage.getItem('token') || '',
+  token : localStorage.getItem('access_token') || '', // token stored in state is access token
   user : {},
+  isAuthenticated: false,
 };
 
 const getters = {
-  isLoggedIn(state) {
-    return !!state.token;
-  },
-  authHeader(state) {
-    return (state.user && state.token) ? {'Authorization' : 'Bearer ' + state.token} : {};
-  }
 
 };
 
 const mutations = {
-  authRequest(state) {
-    state.status = 'working'
+  authRequest(state, constant) {
+    state.status = constant;
+    state.isAuthenticated = false;
   },
-  authSuccess(state, token, user) {
+  authSuccess(state, token, constant) {
+    state.status = constant;
+    state.token = token;
+    state.isAuthenticated = true;
+  },
+  authFailure(state, constant) {
+    state.status = state;
+    state.isAuthenticated = false;
+  },
+  loginSuccess(state, token, user) { // can this function be removed entirely??
     state.status = 'success';
     state.token = token;
     state.user = user;
-  },
-  authError(state) {
-    state.status = 'error';
+    state.isAuthenticated = true; 
   },
   logOut(state) {
     state.status = '';
     state.token = '';
     state.user = {};
+    state.isAuthenticated = false;
   }
 };
 
 const actions = {
-
+  // login needs to retrieve JWT and then dispatch another action to retrieve user.
   async login(context, user) {
-    const url = '';
-    context.commit('authRequest');
-    const response = await axiosAPI.post(url, user);
-    if (response.status === 200) {
-      localStorage.setItem('token', response.data.token)
-      context.commit('authSuccess', response.data.token, response.data.user);
-    } else {
-      // eslint-disable-next-line no-console
-      console.log(error, 'Invalid Login');
-      context.commit('authError'); 
-      throw new Error(response.status);     
-    } 
+    context.commit('authRequest', LOGIN_REQUEST);
+    try {
+      const response = await axiosInstance.post(API_LOGIN, user);
+      if (response.status === 200) {
+        localStorage.setItem('access_token', response.data.access_token);
+        localStorage.setItem('refresh_token', response.data.refresh_token);
+        context.commit('authSuccess', response.data.access_token, LOGIN_SUCCESS);
+        context.dispatch('getUser');
+      } else {
+        context.commit('authFailure', LOGIN_FAILURE); 
+        throw new Error(response.status);     
+      }  
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+        context.commit('authFailure', LOGIN_FAILURE); 
+        throw new Error("Login Failed ");     
+    }
   },
+
+  async getUser(context) {
+    
+  }
 };
 
 export default {

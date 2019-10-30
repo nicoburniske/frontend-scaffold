9618d498bbcb20ec8dbdebf1888658de55e8f093
Nick Burniske <nicoburniske@gmail.com>
2019-10-30 11:45:00 -0400

    added signup method and removed redundant data
    Removed boolean array 'valid' and reincorporated features reliant upon 'valid' with existing string array 'inputError'.

--- src/components/Login/SignupForm.vue
+++ src/components/Login/SignupForm.vue
@@ -3,15 +3,15 @@
     <div>
       <div>
          <input v-model="username" @focus="resetSubmit" type="text" placeholder="Username" >
-         <span v-show="!valid[0]">{{inputError[0]}} </span>
+         <span v-show="!!inputError[0]">{{inputError[0]}} </span>
       </div>
       <div>
          <input v-model="email" @focus="resetSubmit" type="text" placeholder="Email" >
-         <span v-show="!valid[1]"> {{inputError[1]}} </span>
+         <span v-show="!!inputError[1]"> {{inputError[1]}} </span>
       </div>
       <div>
         <input v-model="password1" @focus="resetSubmit" type="password" placeholder="Password">
-        <span v-show="!valid[2]"> {{inputError[2]}} </span>
+        <span v-if="!!inputError[2]"> {{inputError[2]}} </span>
         <!-- eslint-disable-next-line max-len -->
         <input v-model="password2" @focus="resetSubmit" type="password" placeholder="Re-type Password">
       </div>
@@ -32,7 +32,6 @@ export default {
       email: '',
       password1: '',
       password2: '',
-      valid: [false, false, false],
       inputError: ['', '', ''],
       submitted: false,
     };
@@ -46,7 +45,6 @@ export default {
       this.email = '';
       this.password1 = '';
       this.password2 = '';
-      this.valid = [false, false, false];
       this.inputError = ['', '', ''];
     },
     validate() {
@@ -55,54 +53,51 @@ export default {
       this.validatePassword();
     },
     validateUser() {
-      // validate Username
       if (!this.username) {
-        this.valid.splice(0, 1, false);
         this.inputError.splice(0, 1, 'Username cannot be empty');
       } else if (this.username.length > 20) {
-        this.valid.splice(0, false);
         this.inputError.splice(0, 1, 'Username is too long (Cannot exceed 20 characters)');
       } else {
-        this.valid.splice(0, true);
         this.inputError.splice(0, 1, '');
       }
     },
     validateEmail() {
-      // validate email
       // eslint-disable-next-line no-useless-escape
       const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if (!this.email) {
-        this.valid.splice(1, 1, false);
         this.inputError.splice(1, 1, 'Email cannot be empty');
       } else if (!emailRegex.test(this.email)) {
-        this.valid.splice(1, 1, false);
         this.inputError.splice(1, 1, 'Invalid Email');
       } else {
-        this.valid.splice(1, 1, true);
         this.inputError.splice(1, 1, '');
       }
     },
     validatePassword() {
-      // validate password
       if (!this.password1 || !this.password2) {
-        this.valid.splice(2, 1, false);
         this.inputError.splice(2, 1, 'Password cannot be empty');
       } else if (this.password1 !== this.password2) {
-        this.valid.splice(2, 1, false);
         this.inputError.splice(2, 1, 'Passwords do not match');
       } else {
-        this.valid.splice(2, 1, true);
         this.inputError.splice(2, 1, '');
       }
     },
-    signup() {
+    async signup() {
       this.submitted = true;
       this.validate();
       // eslint-disable-next-line no-console
       console.log('validating');
-      if (this.valid.reduce((acc, curr) => acc && curr, true)) {
+      if (this.inputError.reduce((acc, curr) => acc && curr === '', true)) {
         // eslint-disable-next-line no-console
         console.log('valid input');
+        const user = { username: this.username, email: this.email, password: this.password1 };
+        try {
+          await this.$store.dispatch('signup', user);
+          this.resetInput();
+          this.$router.push('/journal');
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.log(`${error} from signup form`);
+        }
       }
     },
   },

a7a048d96a4b65f45d6f13f8aee2e9168e2ce580
Nick Burniske <nicoburniske@gmail.com>
2019-10-30 11:35:14 -0400

    added comments and changed axiosInstance header property name

--- src/auth/axiosInstance.js
+++ src/auth/axiosInstance.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 import store from '../store/store';
-import { TOKEN_REFRESH_FAILURE, TOKEN_REFRESH_SUCCESS, API_DOMAIN, API_LOGIN, TOKEN_REFRESH_REQUEST } from './constants';
+import { TOKEN_REFRESH_FAILURE, TOKEN_REFRESH_SUCCESS, API_DOMAIN, TOKEN_REFRESH_REQUEST, API_REFRESH_TOKEN } from './constants';
 
 const axiosInstance = axios.create({
   baseURL: API_DOMAIN,
   timeout: 5000,
   headers: {
-    contentType: 'application/json',
-    accept: 'application/json',
-    Authorization: localStorage.getItem('access_token'),
+    'Content-Type': 'application/json',
+    'X-Access-Token': localStorage.getItem('access_token'),
   },
 });
 
@@ -22,11 +21,14 @@ axiosInstance.interceptors.response.use(
       store.commit('authRequest', TOKEN_REFRESH_REQUEST);
 
       try {
-        const { data } = await axiosInstance.post(API_LOGIN, { refreshToken });
+        // should I have a check for status code?
+        const { data } = await axiosInstance.post(API_REFRESH_TOKEN, { refreshToken });
         store.commit('authSuccess', data.refresh_token, TOKEN_REFRESH_SUCCESS);
         localStorage.setItem('access_token', data.access_token);
-        axiosInstance.defaults.headers.Authorization = data.access_token;
-        pastRequest.headers.Authorization = data.access_token; // which of these do I use? Both?
+        // all future requests will use the new access token.
+        axiosInstance.defaults.headers['X-Access-Token'] = data.access_token;
+        // redo current failed request with new access token
+        pastRequest.headers['X-Access-Token'] = data.access_token;
         return axios(pastRequest);
       } catch (err) {
         // eslint-disable-next-line no-console

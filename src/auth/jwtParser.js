71ff69dbc7cc222f19a12b50b2cba75f6efdee32
Nicolas Burniske <nicoburniske@gmail.com>
2019-11-05 18:01:18 -0500

    added function to determine if a jwt has expired

--- src/auth/jwtParser.js
+++ src/auth/jwtParser.js
@@ -1,4 +1,4 @@
-export function getPayload (token) {
+export function getTokenPayload(token) {
   try {
     return JSON.parse(atob(token.split('.')[1]));
   } catch (e) {
@@ -6,10 +6,16 @@ export function getPayload (token) {
   }
 }
 
-export function getTokenHeader (token) {
+export function getTokenHeader(token) {
   try {
     return JSON.parse(atob(token.split('.')[0]));
   } catch (e) {
     return null;
   }
+}
+
+export function isJWTValid(jwt) {
+  const body = getTokenPayload(jwt);
+  const now = new Date();
+  return body !== null && body.expiration < now.getTime();
 }

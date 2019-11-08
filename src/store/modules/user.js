65541140fd060f890e7b4e6a2db39847debe0d2f
Nick Burniske <nicoburniske@gmail.com>
2019-11-08 15:59:57 -0500

    modified for testing

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-shadow */
-import axiosInstance from '../../auth/axiosInstance';
 import {
-  API_LOGIN, API_SIGNUP, API_REFRESH_TOKEN,
   LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE,
-  SIGNUP_REQUEST, SIGNUP_FAILURE, SIGNUP_SUCCESS,
+  SIGNUP_REQUEST, SIGNUP_SUCCESS, SIGNUP_FAILURE,
   LOGOUT_REQUEST, LOGOUT_SUCCESS,
   PERSIST_SUCCESS, PERSIST_REQUEST, PERSIST_FAILURE,
 } from '../../auth/constants';
-import { isJWTValid } from '../../auth/jwtUtils';
+import jwtUtils from '../../auth/jwtUtils';
 import tokenService from '../../auth/tokenService';
+import userApi from '../../auth/api/userApi';
 
 const state = {
   status: '',
@@ -38,7 +37,7 @@ const mutations = {
     state.isAuthenticated = true;
   },
   /**
-   * Used to set the state status to a failure constant.
+   * Used to set the state status to a failure constant. See constants.js.
    */
   authFailure(state, status) {
     state.status = status;
@@ -52,7 +51,7 @@ const mutations = {
     state.status = payload;
     state.user = {};
     state.isAuthenticated = false;
-    delete axiosInstance.defaults.headers['X-Access-Token'];
+    userApi.deleteAccessTokenHeader();
     tokenService.removeAccessToken();
     tokenService.removeRefreshToken();
   },
@@ -62,51 +61,47 @@ const actions = {
   async login(context, user) {
     context.commit('authRequest', LOGIN_REQUEST);
     try {
-      const response = await axiosInstance.post(API_LOGIN, user);
+      const response = await userApi.login(user);
       if (response.status === 201) {
         context.commit('authSuccess', LOGIN_SUCCESS);
         tokenService.setAccessToken(response.data.access_token);
         tokenService.setRefreshToken(response.data.refresh_token);
-        axiosInstance.defaults.headers['X-Access-Token'] = response.data.access_token;
-        // context.commit('setUser');
+        userApi.setAccessTokenHeader(response.data.access_token);
+        context.dispatch('getUser');
       } else {
-        context.commit('authFailure', LOGIN_FAILURE);
-        throw new Error(response.status);
+        throw new Error(`Login failed with response status ${response.status}`);
       }
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error(error);
       context.commit('authFailure', LOGIN_FAILURE);
-      throw new Error('Login Failed');
     }
   },
   async signup(context, user) {
     context.commit('authRequest', SIGNUP_REQUEST);
     try {
-      const response = await axiosInstance.post(API_SIGNUP, user);
+      const response = await userApi.signup(user);
       if (response.status === 201) {
         context.commit('authSuccess', SIGNUP_SUCCESS);
         tokenService.setAccessToken(response.data.access_token);
         tokenService.setRefreshToken(response.data.refresh_token);
-        axiosInstance.defaults.headers['X-Access-Token'] = response.data.access_token;
+        userApi.setAccessTokenHeader(response.data.access_token);
         // context.commmit('setUser');
+      } else {
+        throw new Error(`Signup failed with response status ${response.status}`);
       }
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error(error);
       context.commit('authFailure', SIGNUP_FAILURE);
-      throw new Error('Signup Failed');
     }
   },
   async logout(context) {
     context.commit('authRequest', LOGOUT_REQUEST);
     try {
-      const response = await axiosInstance.delete(API_LOGIN,
-        {
-          access_token: context.state.access_token,
-          refresh_token: context.state.refresh_token,
-        });
       context.commit('logout');
+      const response = await userApi.logout(tokenService.getAccessToken(),
+        tokenService.getRefreshToken());
       if (response.status !== 204) {
         throw new Error(`Logout failed with response status ${response.status}`);
       }
@@ -119,16 +114,15 @@ const actions = {
     const access = tokenService.getAccessToken();
     const refresh = tokenService.getRefreshToken();
 
-    if (isJWTValid(access)) {
+    if (jwtUtils.isJWTValid(access)) {
       context.commit('authSuccess', PERSIST_SUCCESS);
-    } else if (isJWTValid(refresh)) {
+    } else if (jwtUtils.isJWTValid(refresh)) {
       context.commit('authRequest', PERSIST_REQUEST);
       try {
-        const { data } = await axiosInstance.post(API_REFRESH_TOKEN, { refresh_token: refresh });
+        const { data } = await userApi.refresh(refresh);
         context.commit('authSuccess', PERSIST_SUCCESS);
         tokenService.setAccessToken(data.access_token);
-        // all future requests will use the new access token.
-        axiosInstance.defaults.headers['X-Access-Token'] = data.access_token;
+        userApi.setAccessTokenHeader(data.access_token);
       } catch (error) {
         // eslint-disable-next-line no-console
         console.error(error);
@@ -138,11 +132,14 @@ const actions = {
       context.commit('logout', PERSIST_FAILURE);
     }
   },
-  /**
-    async getUser(context) {
-
-    },
-  * */
+  async getUser() {
+    try {
+      await userApi.getUser();
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('YEEY', error.status);
+    }
+  },
 };
 
 export default {

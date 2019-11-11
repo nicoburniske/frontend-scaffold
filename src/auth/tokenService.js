1b38a646d6cb090820a5016976a5a1f5ca04712a
Nicolas Burniske <nicoburniske@gmail.com>
2019-11-10 21:39:57 -0500

    removed return statement from setter

--- src/auth/tokenService.js
+++ src/auth/tokenService.js
@@ -11,7 +11,7 @@ const tokenService = {
     return localStorage.getItem(ACCESS_TOKEN_KEY);
   },
   setAccessToken(access) {
-    return localStorage.setItem(ACCESS_TOKEN_KEY, access);
+    localStorage.setItem(ACCESS_TOKEN_KEY, access);
   },
   removeAccessToken() {
     localStorage.removeItem(ACCESS_TOKEN_KEY);
@@ -20,7 +20,7 @@ const tokenService = {
     return localStorage.getItem(REFRESH_TOKEN_KEY);
   },
   setRefreshToken(refresh) {
-    return localStorage.setItem(REFRESH_TOKEN_KEY, refresh);
+    localStorage.setItem(REFRESH_TOKEN_KEY, refresh);
   },
   removeRefreshToken() {
     localStorage.removeItem(REFRESH_TOKEN_KEY);
